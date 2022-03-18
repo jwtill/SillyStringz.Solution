33532e39112d54517d82150698cc3a5d5bf112db
Josh Tillinghast <jwtill@icloud.com>
2022-03-18 11:15:51 -0700

    Add Post create route and corresponding view for machines

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -27,5 +27,18 @@ namespace Factory.Controllers
       return View();
     }
 
+    [HttpPost]
+    public ActionResult Create(Machine machine, int EngineerId)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      if (EngineerId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+
   }
 }
